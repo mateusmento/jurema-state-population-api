4fb4432dab13c7359d31d200b6b85a56509eadb5
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-03-19 14:45:14 -0300

    Initial commit

---
+++ src/main/java/com/jurema/statepopulationapi/JuremaStatePopulationApiApplication.java
@@ -0,0 +1,13 @@
+package com.jurema.statepopulationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuremaStatePopulationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuremaStatePopulationApiApplication.class, args);
+	}
+
+}
