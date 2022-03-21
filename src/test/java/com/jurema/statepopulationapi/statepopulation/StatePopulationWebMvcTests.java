d4a0480849d4c021b2803c95416b2552965efd6f
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-03-20 22:51:38 -0300

    Test state population listing from database

--- src/test/java/com/jurema/statepopulationapi/statepopulation/StatePopulationWebMvcTests.java
+++ src/test/java/com/jurema/statepopulationapi/statepopulation/StatePopulationWebMvcTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -19,6 +21,8 @@ public class StatePopulationWebMvcTests {
 	private MockMvc mvc;
 
 	@Test
+	@Sql("classpath:datasets/state-populations.sql")
+	@Sql(statements = "delete from state_population", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 	void shouldListStatePopulation() throws Exception {
 		// given
 		var request = get("/state-populations");
@@ -29,7 +33,13 @@ public class StatePopulationWebMvcTests {
 		// then
 		response.andExpectAll(
 			status().isOk(),
-			jsonPath("$", hasSize(0))
+			jsonPath("$", hasSize(2)),
+			jsonPath("$[0].id", equalTo(1)),
+			jsonPath("$[0].uf", equalTo("SP")),
+			jsonPath("$[0].populacao", equalTo(1000)),
+			jsonPath("$[1].id", equalTo(2)),
+			jsonPath("$[1].uf", equalTo("RJ")),
+			jsonPath("$[1].populacao", equalTo(2000))
 		);
 	}
 }
